OpenRTX/OpenRTX

CS7000: radio driver

openrtx/src/rtx/OpMode_M17.cpp
@@ -96,6 +96,8 @@ void OpMode_M17::update(rtxStatus_t *const status, const bool newCfg)
     //
     invertTxPhase = (mod17CalData.bb_tx_invert == 1) ? true : false;
     invertRxPhase = (mod17CalData.bb_rx_invert == 1) ? true : false;
+    #elif defined(PLATFORM_CS760)
+    invertTxPhase = true;
     #endif
 
     // Main FSM logic

jonz-secops/OpenRTX_OpenRTX

openrtx/src/rtx/OpMode_M17.cpp
@@ -96,6 +96,8 @@ void OpMode_M17::update(rtxStatus_t *const status, const bool newCfg)
     //
     invertTxPhase = (mod17CalData.bb_tx_invert == 1) ? true : false;
     invertRxPhase = (mod17CalData.bb_rx_invert == 1) ? true : false;
+    #elif defined(PLATFORM_CS7000)
+    invertTxPhase = true;
     #endif
 
     // Main FSM logic